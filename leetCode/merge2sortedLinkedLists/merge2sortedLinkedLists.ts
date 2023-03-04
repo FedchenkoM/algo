db5dda293b27a087df485e91f0886cfb700cc35d
Fedchenko Mike <fedchenko@yahoo.com>
2023-03-04 15:30:35 +0300

    merge 2 sorted linked lists

---
+++ leetCode/merge2sortedLinkedLists/merge2sortedLinkedLists.ts
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/
+
+function ListNode(val, next) {
+	this.val = val === undefined ? 0 : val;
+	this.next = next === undefined ? null : next;
+}
+
+const l2 = { val: 1, next: { val: 2, next: { val: 4, next: null } } };
+const l1 = { val: 1, next: { val: 3, next: { val: 4, next: null } } };
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+	if (list1 === null) return list2;
+	if (list2 === null) return list1;
+
+	let link = new ListNode(null, null);
+	let head = link;
+
+	while (list1 !== null && list2 !== null) {
+		if (list1.val < list2.val) {
+			link.next = list1;
+			list1 = list1.next;
+		} else {
+			link.next = list2;
+			list2 = list2.next;
+		}
+
+		link = link.next;
+	}
+
+	if (list1 == null) {
+		link.next = list2;
+	} else {
+		link.next = list1;
+	}
+
+	return head.next;
+};
