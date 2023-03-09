1ffa14736aef1a6e3463ca7910062a5ea94fd1ff
Fedchenko Mike <fedchenko@yahoo.com>
2023-03-09 17:26:02 +0300

    removeElement

--- leetCode/removeElement/removeElement.js
+++ leetCode/removeElement/removeElement.js
@@ -1,24 +1,22 @@
-//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+//https://leetcode.com/problems/remove-element/
 
 const nums = [-3, -1, 0, 0, 0, 3, 3];
 const nums1 = [1, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 55, 55];
 
 /**
  * @param {number[]} nums
+ * @param {number} val
  * @return {number}
  */
-var removeDuplicates = function (nums) {
+var removeElement = function (nums, val) {
   let i = 0;
   for (let k = 0; k < nums.length;) {
-    if (nums[i] !== nums[k]) {
-      i++;
+    if (nums[k] !== val) {
       nums[i] = nums[k];
+      i++;
     }
     k++;
   }
 
-  return ++i;
+  return i;
 };
-
-
-
