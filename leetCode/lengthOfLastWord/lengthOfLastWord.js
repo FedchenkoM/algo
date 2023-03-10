e87763eed2e8705ec4db1662dfbf1b7708422e9b
Fedchenko Mike <fedchenko@yahoo.com>
2023-03-10 12:05:49 +0300

    lengthOfLastWord

---
+++ leetCode/lengthOfLastWord/lengthOfLastWord.js
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/length-of-last-word/
+
+const s = "dodo pizza    ";
+const s2 = "dodo";
+const s3 = "";
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  for (let i = k = s.length - 1; i >= 0; i--, k--) {
+    if (s[i] !== ' ') {
+      while (s[k] !== ' ' && k > -1) {
+        k--;
+      }
+      return i - k;
+    }
+  }
+
+  return 0;
+};
+
+
