48ad7ced88501d0f22c70e906c21c52d5c4bc029
Fedchenko Mike <fedchenko@yahoo.com>
2023-03-22 16:55:47 +0300

    move TREES

--- src/leetCode/sameTree/sameTree.js
+++ src/leetCode/sameTree/sameTree.js
@@ -1,74 +1,5 @@
 // https://leetcode.com/problems/same-tree/
-
-const tree = {
-    val: 52,
-    left: {
-        val: 46,
-        left: {
-            val: 41,
-            left: null,
-            right: {
-                val: 44,
-                left: null,
-                right: { val: 45, left: null, right: null },
-            },
-        },
-        right: {
-            val: 49,
-            left: { val: 48, left: null, right: null },
-            right: { val: 50, left: null, right: null },
-        },
-    },
-    right: {
-        val: 60,
-        left: { val: 55, left: null, right: null },
-        right: { val: 68, left: null, right: null },
-    },
-};
-
-const tree2 = {
-    val: 52,
-    left: {
-        val: 46,
-        left: {
-            val: 41,
-            left: null,
-            right: {
-                val: 44, // sasas
-                left: null,
-                right: { val: 45, left: null, right: null },
-            },
-        },
-        right: {
-            val: 49,
-            left: { val: 48, left: null, right: null },
-            right: { val: 50, left: null, right: null },
-        },
-    },
-    right: {
-        val: 60,
-        left: { val: 55, left: null, right: null },
-        right: { val: 68, left: null, right: null },
-    },
-};
-
-const tree3 = {
-    val: 1,
-    left: { val: 2, left: null, right: null },
-    right: { val: 3, left: null, right: null },
-};
-
-const tree4 = {
-    val: 2,
-    left: null,
-    right: { val: 4, left: null, right: null },
-};
-
-const tree5 = {
-    val: 2,
-    left: { val: 3, left: null, right: null },
-    right: { val: 4, left: null, right: null },
-};
+import { TREES } from './trees';
 
 const isSameTree = function (p, q) {
     const stack = [];
@@ -93,3 +24,5 @@ const isSameTree = function (p, q) {
 
     return true;
 };
+
+isSameTree(TREES.tree, TREES.tree2);
