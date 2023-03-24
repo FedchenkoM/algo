b0742b0e184e594aab38a0119f7babae5d9ffd9f
Fedchenko Mike <fedchenko@yahoo.com>
2023-03-24 22:28:07 +0300

    reverseLinkedList

---
+++ src/leetCode/easy/reverseLinkedList/reverseLinkedList.js
@@ -0,0 +1,25 @@
+const linkedList = {
+    val: 1,
+    next: {
+        val: 2,
+        next: { val: 3, next: { val: 4, next: { val: 5, next: null } } },
+    },
+};
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+    let prev = null;
+    let cur = head;
+
+    while (cur) {
+        const n = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = n;
+    }
+
+    return prev;
+};
