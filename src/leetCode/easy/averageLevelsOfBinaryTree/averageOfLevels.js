e9b099eb7cf7f95265c8acfd50a9c4d0cd3cb5ce
Fedchenko Mike <fedchenko@yahoo.com>
2023-04-29 19:59:59 +0400

    average of levels

---
+++ src/leetCode/easy/averageLevelsOfBinaryTree/averageOfLevels.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const averageOfLevels = function (root) {
+    const queue = [root];
+    const avg = [];
+    while (queue.length) {
+        let sum = 0;
+        const curLength = queue.length;
+        for (let i = 0; i < curLength; i++) {
+            const node = queue.shift();
+            sum += node.val;
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        avg.push(sum / curLength);
+    }
+
+    return avg;
+};
