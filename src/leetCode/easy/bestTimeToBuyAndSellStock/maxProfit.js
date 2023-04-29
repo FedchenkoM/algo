d399f9bda6b0296d51fc72c8c272c446d7e5118a
Fedchenko Mike <fedchenko@yahoo.com>
2023-04-29 19:43:34 +0400

    max profit

--- src/leetCode/easy/bestTimeToBuyAndSellStock/maxProfit.js
+++ src/leetCode/easy/bestTimeToBuyAndSellStock/maxProfit.js
@@ -4,18 +4,4 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function (prices) {
-    let maxProfit = 0;
-    let minPrice = prices[0];
-    for (let i = 1; i < prices.length; i++) {
-        const currentPrice = prices[i];
-        const currentProfit = currentPrice - minPrice;
-        if (currentProfit > maxProfit) {
-            maxProfit = currentProfit;
-        }
-        if (currentPrice < minPrice) {
-            minPrice = currentPrice;
-        }
-    }
-    return maxProfit;
-};
+const maxProfit = function (prices) {};
