f00c2b49da4d1e95df7f6ed70037804928e6132b
Fedchenko Mike <fedchenko@yahoo.com>
2023-05-03 09:43:26 +0400

    preorder traversal

---
+++ src/leetCode/easy/binaryTreePreorderTraversal/preorderTraversal.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const preorderTraversal = function (root) {
+    const result = [];
+
+    if (!root) return result;
+
+    const stack = [root];
+
+    while (stack.length) {
+        for (let i = 0; i < stack.length; i++) {
+            const node = stack.pop();
+
+            result.push(node.val);
+            if (node.right) stack.push(node.right);
+            if (node.left) stack.push(node.left);
+        }
+    }
+    return result;
+};
